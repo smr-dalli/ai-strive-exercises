ade105e3179f45964ced4778ff3a4636082129f8
Sai Mohan Dalli <74708791+smr-dalli@users.noreply.github.com>
2021-04-14 19:45:34 +0200

    Create student.py

---
+++ M8/d3/sql/database/student.py
@@ -0,0 +1,20 @@
+import db_interface as sql
+import sqlite3
+import pandas as pd
+
+student_table = """CREATE TABLE student(
+                           name char(20) NOT NULL,
+                           surname char(20) NOT NULL,
+                           id int NOT NULL PRIMARY KEY,
+                           country char(20),
+                           FOREIGN KEY(id) REFERENCES projects (st)
+                           );
+                         """
+
+sql.sql_query(student_table)
+
+sql.upload_data('student_table','./datasets/student.csv')
+
+df = sql.pandas_select('select * from student_table')
+print(df)
+sql.close()
